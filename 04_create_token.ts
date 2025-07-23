f039135cc10e68fb7efbf6a1dfba8bdf96a97327
Muhammad Salal Qadir <93817435+salalqadir05@users.noreply.github.com>
2025-07-23 12:22:16 +0100

    creating token through the keypair

---
+++ 04_create_token.ts
@@ -0,0 +1,20 @@
+import { getExplorerLink, getKeypairFromEnvironment } from "@solana-developers/helpers";
+import { createMint } from "@solana/spl-token";
+import { clusterApiUrl, Connection } from "@solana/web3.js";
+import 'dotenv/config';
+
+const Keypair = process.env.KEYPAIR;
+
+if(!Keypair) {
+  console.log("Keypair not found in environment variables.");
+}
+
+console.log(`Keypair: ${Keypair}`);
+const connection = new Connection(clusterApiUrl('devnet'));
+const OWNER = getKeypairFromEnvironment('KEYPAIR');
+console.log(`Public key: ${OWNER.publicKey.toBase58()}`);
+const token = await createMint(connection,OWNER, OWNER.publicKey, null, 2);
+const link = getExplorerLink('address', token.toString(), 'devnet');
+
+
+console.log(`Done! created token: ${link}`);
